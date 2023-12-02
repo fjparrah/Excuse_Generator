5c32c570538e70b3cb0c6fbd8354e63a9e6defae
fjparrah <48214359+fjparrah@users.noreply.github.com>
2023-12-02 20:51:58 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Función para generar una excusa aleatoria
+function generarExcusa() {
+  let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'the keys', 'the car'];
+  let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // Obtener elementos aleatorios de cada matriz
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+
+  let excusaGenerada = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+  return excusaGenerada; 
+}
+
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ 
+  let excuseParagraph = document.getElementById('excuse');
+  excuseParagraph.innerHTML = generarExcusa();
+
 };
